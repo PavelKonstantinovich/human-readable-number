e40eaac0d33d1934784303660e78aec291ae222a
PavelKonstantinovich <pashakonstantinovich@gmail.com>
2023-02-14 22:27:37 +0300

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let arrToTwenty = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    let arrToHundred = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+    const getNumToHundred = (n) => n >= 20 ? arrToHundred[Math.floor(n / 10)] + ' ' + arrToTwenty[n % 10] : arrToTwenty[n]
+    return number === 0 ? 'zero' : number > 99 ? (arrToTwenty[Math.floor(number / 100)] + ' hundred ' + getNumToHundred(number % 100)).trim() : (getNumToHundred(number)).trim()
 }
